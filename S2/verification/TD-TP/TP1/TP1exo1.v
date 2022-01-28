b808538672ffd9bca144d0bbcb0e3ffdcc55aa4d
Jérémie <jeremie.bentolila01@etu.umontpellier.fr>
2022-01-28 02:37:31 +0100

    TP1 exo1 fait en COQ (sauf la  maudite Q2)

---
+++ S2/verification/TD-TP/TP1/TP1exo1.v
@@ -0,0 +1,84 @@
+Parameters A B C: Prop.
+
+(* n°1 *)
+Goal A -> B -> A. 
+intros hyp1 hyp2.
+assumption.
+Qed.
+
+(* n°2 *)
+(* Goal (A -> B -> C) -> (A -> B) -> C.
+intros.
+cut A.
+intro.
+pose (H0 H1).
+pose (H H1 b).
+exact c.
+Qed.
+ *)
+
+(* n°3 *)
+Goal A /\ B -> B.
+intro.
+elim H.
+intros.
+assumption.
+Qed.
+
+(* n°4 *)
+Goal B -> A \/ B.
+intro.
+right.
+assumption.
+Qed.
+
+(* n°5 *)
+Goal (A \/ B) -> ( A -> C ) -> ( B -> C) -> C.
+intros.
+elim H.
+intros.
+apply H0.
+assumption.
+intro.
+apply (H1 H2).
+Qed.
+
+(* n°6 *)
+Goal A -> False -> A.
+intro.
+auto.
+Qed.
+
+(* n°7 *)
+Goal False -> A.
+intro.
+destruct H.
+Qed.
+
+(* n°8 *)
+Goal (A <-> B) -> A -> B.
+intros.
+elim H.
+intros.
+apply (H1 H0).
+(*apply H1.
+assumption.*)
+Qed.
+
+(* n°9 *)
+Goal (A <-> B) -> B -> A.
+intros.
+elim H.
+intros.
+apply (H2 H0).
+(*apply H2.
+assumption.*)
+Qed.
+
+(* n°10 *)
+Goal (A -> B) -> (B -> A ) -> (A <-> B).
+intros.
+split.
+apply H.
+apply H0.
+Qed.
